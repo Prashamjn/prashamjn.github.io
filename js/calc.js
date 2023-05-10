36c6ee5f8caa287ca5c0fa6390f5bf22fde8f0af
Prashamjn <prasham2306@gmail.com>
2023-05-10 11:10:36 +0530

    Calculator

--- js/calc.js
+++ js/calc.js
@@ -1,18 +1,17 @@
 function addToResult(value) {
-    document.getElementById('result').value += value;
-  }
-  
-  function clearResult() {
-    document.getElementById('result').value = '';
-  }
-  
-  function clearEntry() {
-    var result = document.getElementById('result').value;
-    document.getElementById('result').value = result.substring(0, result.length - 1);
-  }
-  
-  function calculateResult() {
-    var result = eval(document.getElementById('result').value);
-    document.getElementById('result').value = result;
-  }
-  
+  document.getElementById('result').value += value;
+}
+
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+
+function clearEntry() {
+  var result = document.getElementById('result').value;
+  document.getElementById('result').value = result.substring(0, result.length - 1);
+}
+
+function calculateResult() {
+  var result = eval(document.getElementById('result').value);
+  document.getElementById('result').value = result;
+} 
